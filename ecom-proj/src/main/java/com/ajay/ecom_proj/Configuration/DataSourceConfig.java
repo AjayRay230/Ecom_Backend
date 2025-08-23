0792ee55f1ead1c7f2c223b135adcf3beff75fd7
royajaygit <ajayray2409@gmail.com>
2025-08-23 17:33:30 +0530

    fix database problem

---
+++ ecom-proj/src/main/java/com/ajay/ecom_proj/Configuration/DataSourceConfig.java
@@ -0,0 +1,31 @@
+package com.ajay.ecom_proj.Configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+
+import javax.sql.DataSource;
+import java.net.URI;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Value("${DATABASE_URL}")
+    private String databaseUrl;
+
+    @Bean
+    public DataSource dataSource() throws Exception {
+        URI dbUri = new URI(databaseUrl);
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String url = "jdbc:postgresql://" + dbUri.getHost() + ":" + dbUri.getPort() + dbUri.getPath();
+
+        return DataSourceBuilder.create()
+                .url(url)
+                .username(username)
+                .password(password)
+                .build();
+    }
+}
